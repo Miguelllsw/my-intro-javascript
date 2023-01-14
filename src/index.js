72978eecc12d63787adb49b6ec3977594cb926b0
migue <miguelhernandez.gonzalez28@gmail.com>
2023-01-14 19:48:51 -0300

    sss

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
     <App />
-  </React.StrictMode>
+  </FirebaseAppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
