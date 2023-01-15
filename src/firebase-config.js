87cab864fe7b3d2f6a7b0ea2374f1c0fe4696413
migue <miguelhernandez.gonzalez28@gmail.com>
2023-01-14 22:55:57 -0300

    nnn

--- src/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export default{
-    apiKey: "AIzaSyCYT806Pgx9YtGEwNrw__p0v8mQK-cEanY",
-    authDomain: "asisappweb.firebaseapp.com",
-    projectId: "asisappweb",
-    storageBucket: "asisappweb.appspot.com",
-    messagingSenderId: "336619382763",
-    appId: "1:336619382763:web:d4bb4759faf6aa6441dbae"
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-function app(){
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from "./reportWebVitals";
+import AplicacionSnoopy from "./pages/inicio";
+import "./index.css";
 
 
-
-return(
-<div>holaaaaa</div>
-  
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-);}
+root.render(
+  <StrictMode>
+    
+      <AplicacionSnoopy/>
+    
+  </StrictMode>
+);
 
-export default app;
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
