61c758193413dbf5c806868a0723673091549efd
migue <miguelhernandez.gonzalez28@gmail.com>
2023-03-03 19:12:59 -0300

    ddd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { LoginButton } from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* <LoginButton />           */}
+          </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
